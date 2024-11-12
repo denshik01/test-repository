79fa188cf29ce83dddb32ae5bddc763b1fb884f5
denshik01 <d902442@gmail.com>
2024-11-12 14:16:40 +0300

    Add name to App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const greeting = "Это тестовый проект ФИО";
+  const greeting = "Это тестовый проект Цвигула Денис Андреевич";
   // eslint-disable-next-line no-console
   console.log('This is App ');
   return (
